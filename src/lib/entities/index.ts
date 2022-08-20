7c4ed27fbdb5289260abd3fe089368e9bea1913e
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-08-20 02:13:11 +0200

    Add entity index

---
+++ src/lib/entities/index.ts
@@ -0,0 +1,19 @@
+import { Person } from 'src/lib/entities/Person';
+import { VehicleInformation } from 'src/lib/entities/VehicleInformation';
+import { Vehicle } from 'src/lib/entities/Vehicle';
+import { Balloon } from 'src/lib/entities/Balloon';
+import { Car } from 'src/lib/entities/Car';
+import { VehicleGroup } from 'src/lib/entities/VehicleGroup';
+import { Flight } from 'src/lib/entities/Flight';
+import { Project } from 'src/lib/entities/Project';
+
+export {
+    Balloon,
+    Car,
+    Flight,
+    Person,
+    Project,
+    Vehicle,
+    VehicleGroup,
+    VehicleInformation,
+};
