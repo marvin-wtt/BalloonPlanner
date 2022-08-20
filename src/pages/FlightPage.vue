a919792935e8918dbfde698e35c1c13cec88213d
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-08-20 02:16:17 +0200

    Update style

--- src/pages/FlightPage.vue
+++ src/pages/FlightPage.vue
@@ -1,19 +1,17 @@
 <template>
   <q-page padding>
-
     <!-- content -->
     <template v-if="flight !== undefined">
-
       <div class="row">
         <vehicle-group-component
-        v-for="group in flight.vehicleGroups"
-        :key="group.id"
-        :data="group"
+          v-for="group in flight.vehicleGroups"
+          :key="group.id"
+          :data="group"
         />
       </div>
     </template>
 
-<!--    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>-->
+    <!--    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>-->
     <q-drawer v-model="rightDrawerOpen" side="right" :overlay="false" bordered>
       <!-- drawer content -->
       <div class="column">
@@ -25,30 +23,35 @@
       </div>
     </q-drawer>
 
-<!--    <div class="q-px-sm q-py-lg">-->
-<!--      <div class="column items-center" style="margin-top: 100px; margin-bottom: 100px;">-->
-<!--        <q-fab color="secondary" push icon="add" direction="left">-->
-<!--          <q-fab-action color="green" text-color="black" icon="balloon"/>-->
-<!--        </q-fab>-->
-<!--        <q-fab color="secondary" push icon="add" direction="left">-->
-<!--          <q-fab-action color="blue" text-color="black" icon="balloon"/>-->
-<!--        </q-fab>-->
-<!--      </div>-->
-<!--    </div>-->
+    <!--    <div class="q-px-sm q-py-lg">-->
+    <!--      <div class="column items-center" style="margin-top: 100px; margin-bottom: 100px;">-->
+    <!--        <q-fab color="secondary" push icon="add" direction="left">-->
+    <!--          <q-fab-action color="green" text-color="black" icon="balloon"/>-->
+    <!--        </q-fab>-->
+    <!--        <q-fab color="secondary" push icon="add" direction="left">-->
+    <!--          <q-fab-action color="blue" text-color="black" icon="balloon"/>-->
+    <!--        </q-fab>-->
+    <!--      </div>-->
+    <!--    </div>-->
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { useQuasar } from 'quasar'
+import { useQuasar } from 'quasar';
 import VehicleGroupComponent from 'src/components/VehicleGroupComponent.vue';
-import { onBeforeRouteUpdate, RouteParams, useRoute, useRouter } from 'vue-router';
+import {
+  onBeforeRouteUpdate,
+  RouteParams,
+  useRoute,
+  useRouter,
+} from 'vue-router';
 import { useProjectStore } from 'stores/project-store';
 import { storeToRefs } from 'pinia';
 
 export default defineComponent({
   name: 'FlightPage',
-  components: {VehicleGroupComponent},
+  components: { VehicleGroupComponent },
   setup() {
     const $q = useQuasar();
     const route = useRoute();
@@ -62,19 +65,19 @@ export default defineComponent({
       rightDrawerOpen.value = !rightDrawerOpen.value;
     }
 
-    const {project} = storeToRefs(projectStore);
+    const { project } = storeToRefs(projectStore);
     if (project.value === null) {
       $q.notify({
         type: 'warning',
-        message: 'No project found.'
-      })
-      router.push({name: 'projects'});
-      return {flight};
+        message: 'No project found.',
+      });
+      router.push({ name: 'projects' });
+      return { flight };
     }
 
     if (Array.isArray(route.params.flight)) {
-      router.push({name: 'projects'}); // TODO change route
-      return {flight};
+      router.push({ name: 'projects' }); // TODO change route
+      return { flight };
     }
 
     updateFlightPage(route.params);
@@ -83,10 +86,10 @@ export default defineComponent({
       // TODO Maybe update the store?
       $q.notify({
         type: 'warning',
-        message: 'Flight does not exist.'
+        message: 'Flight does not exist.',
       });
-      router.push({name: 'projects'});
-      return {flight};
+      router.push({ name: 'projects' });
+      return { flight };
     }
 
     function updateFlightPage(params: RouteParams) {
@@ -94,7 +97,9 @@ export default defineComponent({
       if (Array.isArray(params.fligh)) return;
 
       const flightId = Number(route.params.flight);
-      flight.value = project.value?.flights.find(value => value.id == flightId)
+      flight.value = project.value?.flights.find(
+        (value) => value.id == flightId
+      );
     }
 
     onBeforeRouteUpdate((to, from) => {
