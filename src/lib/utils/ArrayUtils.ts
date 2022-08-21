86245bb0a1b2074324342f16aa1b0ebadf6311e4
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-08-21 02:25:55 +0200

    Fixes utils export

--- src/lib/utils/ArrayUtils.ts
+++ src/lib/utils/ArrayUtils.ts
@@ -1,4 +1,4 @@
-function shuffle<Type>(a: Array<Type>): Array<Type> {
+export function shuffle<Type>(a: Array<Type>): Array<Type> {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
