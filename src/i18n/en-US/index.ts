a1bb4c932cee3ad1b181c7c2e43de676808e7b7d
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-09-22 01:57:48 +0200

    fix: Vehicle dialog capacity hint

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -11,28 +11,37 @@ export default {
   car: 'Car | Cars',
   create: 'Create',
 
-  dialog_edit_balloon_title: 'Create a new balloon',
-  dialog_edit_balloon_hint_capacity: 'Maximum amout of passengers without the operator',
-  dialog_edit_balloon_validation_type: 'Please select a vehicle type',
-  dialog_edit_balloon_validation_name_required: 'Vehicle name is required',
-  dialog_edit_balloon_validation_name_unique: 'Vehicle name must be unique',
-  dialog_edit_balloon_validation_capacty: 'Please type a positive number',
+  dialog_edit_person_title: 'Add a new person',
+  dialog_edit_person_hint_flights: 'Number of flights previous to this flight',
+  dialog_edit_person_validation_name_required: 'Please tyoe a name',
+
+  dialog_edit_vehicle_title: 'Create a new vehicle',
+  dialog_edit_vehicle_hint_capacity: 'Maximum amout of passengers including the operator',
+  dialog_edit_vehicle_validation_type: 'Please select a vehicle type',
+  dialog_edit_vehicle_validation_name_required: 'Vehicle name is required',
+  dialog_edit_vehicle_validation_name_unique: 'Vehicle name must be unique',
+  dialog_edit_vehicle_validation_capacty: 'Please type a positive number',
 
   drop_here_or_create: 'Drop an item here or create a new one',
 
   flight: 'Flight | Flights',
+  french: 'French',
+
+  german: 'German',
 
   hint_balloon_capacity: 'Amount of passengers without the pilot',
 
   list_empty: 'There are currently no items in this list',
 
   name: 'Name',
+  nationality: 'Nationality',
 
   overview: 'Overview',
 
   person: 'Person | People',
 
   success: 'Action was successful',
+  supervisor: 'Supervisor',
 
   vehicle_type: 'Vehicle type',
 };
