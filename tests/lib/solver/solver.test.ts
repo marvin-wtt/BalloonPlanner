caef01d7d059b72cf8e91d0cc76f513d14dfc60a
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-11-13 01:11:18 +0100

    test(solver): Add solver tests

---
+++ tests/lib/solver/solver.test.ts
@@ -0,0 +1,172 @@
+import { faker } from '@faker-js/faker';
+
+import { Balloon, Car, Flight, Person } from 'src/lib/entities';
+import { solve } from 'src/lib/solver/solver';
+
+it('creates a vehicle group for every balloon', async () => {
+  const p1 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p2 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p3 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p4 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p5 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p6 = new Person(faker.name.firstName(), faker.locale, true, 0);
+
+  const b1 = new Balloon('D-OAAA', 3, [p1]);
+  const b2 = new Balloon('D-OAAB', 3, [p2]);
+  const b3 = new Balloon('D-OAAC', 3, [p3]);
+
+  const c1 = new Car('Car1', 9, [p4]);
+  const c2 = new Car('Car2', 9, [p5]);
+  const c3 = new Car('Car3', 9, [p6]);
+
+  const f = new Flight([b1, b2, b3], [c1, c2, c3], [p1, p2, p3, p4, p5, p6]);
+
+  const result = await solve(f);
+
+  expect(result.vehicleGroups.length).toBe(3);
+  expect(
+    result.vehicleGroups.find((value) => value.balloon.id == b1.id)
+  ).toBeDefined();
+  expect(
+    result.vehicleGroups.find((value) => value.balloon.id == b2.id)
+  ).toBeDefined();
+  expect(
+    result.vehicleGroups.find((value) => value.balloon.id == b3.id)
+  ).toBeDefined();
+});
+
+it('adds a pilot to every balloon', async () => {
+  const p1 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p2 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p3 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p4 = new Person(faker.name.firstName(), faker.locale, true, 0);
+
+  const b1 = new Balloon('D-OAAA', 3, [p1]);
+  const b2 = new Balloon('D-OAAB', 3, [p2]);
+
+  const c1 = new Car('Car1', 9, [p3]);
+  const c2 = new Car('Car2', 9, [p4]);
+
+  const f = new Flight([b1, b2], [c1, c2], [p1, p2, p3, p4]);
+
+  const result = await solve(f);
+
+  expect(result.vehicleGroups[0].balloon.operator).toBeDefined();
+  expect(
+    result.vehicleGroups[0].balloon.allowedOperators.includes(
+      result.vehicleGroups[0].balloon.operator!
+    )
+  ).toBeTruthy();
+
+  expect(result.vehicleGroups[1].balloon.operator).toBeDefined();
+  expect(
+    result.vehicleGroups[1].balloon.allowedOperators.includes(
+      result.vehicleGroups[1].balloon.operator!
+    )
+  ).toBeTruthy();
+});
+
+it('increments the flights for every pilot', async () => {
+  const p1 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p2 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p3 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p4 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p5 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p6 = new Person(faker.name.firstName(), faker.locale, true, 0);
+
+  const b1 = new Balloon('D-OAAA', 3, [p1]);
+  const b2 = new Balloon('D-OAAB', 3, [p2]);
+  const b3 = new Balloon('D-OAAC', 3, [p3]);
+
+  const c1 = new Car('Car1', 9, [p4]);
+  const c2 = new Car('Car2', 9, [p5]);
+  const c3 = new Car('Car3', 9, [p6]);
+
+  const f = new Flight([b1, b2, b3], [c1, c2, c3], [p1, p2, p3, p4, p5, p6]);
+
+  const result = await solve(f);
+
+  expect(result.vehicleGroups[0].balloon.operator?.numberOfFlights).toBe(1);
+  expect(result.vehicleGroups[1].balloon.operator?.numberOfFlights).toBe(1);
+  expect(result.vehicleGroups[2].balloon.operator?.numberOfFlights).toBe(1);
+});
+
+it('ignores balloons without operator', async () => {
+  const p1 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p2 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p3 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p4 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p5 = new Person(faker.name.firstName(), faker.locale, true, 0);
+
+  const b1 = new Balloon('D-OAAA', 3, [p1]);
+  const b2 = new Balloon('D-OAAB', 3, [p1, p2]);
+  // Balloon without any operators
+  const b3 = new Balloon('D-OAAC', 3, []);
+  // Duplicate operator and smaller capacity - should therefore be ignored
+  const b4 = new Balloon('D-OAAD', 2, [p1]);
+
+  const c1 = new Car('Car1', 9, [p3]);
+  const c2 = new Car('Car2', 9, [p4]);
+  const c3 = new Car('Car3', 9, [p5]);
+
+  const f = new Flight([b1, b2, b3, b4], [c1, c2], [p1, p2, p3, p4, p5]);
+  const result = await solve(f);
+
+  expect(result.vehicleGroups.length).toBe(2);
+  expect(
+    result.vehicleGroups.find((value) => value.balloon.id === b3.id)
+  ).toBeUndefined();
+  expect(
+    result.vehicleGroups.find((value) => value.balloon.id === b4.id)
+  ).toBeUndefined();
+});
+
+it('adds at least one car to every group', async () => {
+  const p1 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p2 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p3 = new Person(faker.name.firstName(), faker.locale, true, 0);
+  const p4 = new Person(faker.name.firstName(), faker.locale, true, 0);
+
+  const b1 = new Balloon('D-OAAA', 3, [p1]);
+  const b2 = new Balloon('D-OAAB', 3, [p2]);
+
+  const c1 = new Car('Car1', 9, [p3]);
+  const c2 = new Car('Car2', 9, [p4]);
+
+  const f = new Flight([b1, b2], [c1, c2], [p1, p2, p3, p4]);
+
+  const result = await solve(f);
+
+  expect(result.vehicleGroups[0].cars.length).toBe(1);
+  expect(result.vehicleGroups[1].cars.length).toBe(1);
+});
+
+it.todo('it adds each car at most once to a flight');
+
+it.todo('asignes cars to group with sufficiant capacity');
+
+it.todo('distributes crew size between groups');
+
+it.todo('removes groups if not enough cars are available');
+
+it.todo('adds a pilot to every balloon');
+
+it.todo('ignores smallest balloon if no operator available');
+
+it.todo('adds a driver to every car');
+
+it.todo('only uses allowed operators');
+
+it.todo('throws error if the capacity is not sufficiant for everyone');
+
+it.todo('increments the flight for every person inside a balloon');
+
+it.todo('prioritises participants who never flew before');
+
+it.todo('prioritises participants with frewer flights');
+
+it.todo('prioritises teamer who never flew before');
+
+it.todo('prioritises teamer with no flights');
+
+it.todo('prioritises solutions with frewer cars');
