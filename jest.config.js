1b57d5e6527d0df40837f440dfdb56bac7161f94
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-11-13 01:23:36 +0100

    test: Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+
   transform: { '^.+\\.ts?$': 'ts-jest' },
   testEnvironment: 'node',
+  modulePaths: ['<rootDir>'],
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
