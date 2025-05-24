af3fbc8e8b647eee2b1ac5959705648a8e796e27
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-05-24 23:49:23 +0200

    chore: Add build scripts

---
+++ scripts/build-python-app.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Change to the directory of this script, then into src-python
+cd /d %~dp0..\src-python
+
+REM Activate the virtual environment
+call .venv\Scripts\activate
+
+REM Build the Python application
+call pyinstaller ^
+      --onefile ^
+      --distpath ../dist/python ^
+      --workpath ../dist/python/pyinstaller ^
+      --specpath ../dist/python/pyinstaller ^
+      ./run_balloon_solver.py
+
+REM Go to root directory
+cd /d %~dp0..\
+
+REM Build electron app
+call npm run build
+
---
+++ scripts/buildl-python-app.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status\set -e
+
+# Change to the directory containing this script, then into src-python
+dir=$(dirname "$0")
+cd "$dir/src-python"
+
+# Activate the virtual environment
+# shellcheck source=/dev/null
+source .venv/bin/activate
+
+# Build the Python application
+pyinstaller \
+      --onefile \
+      --distpath ../dist/python \
+      --workpath ../dist/python/pyinstaller \
+      --specpath ../dist/python/pyinstaller \
+      ./run_balloon_solver.py
+
+cd ../
+npm run build
